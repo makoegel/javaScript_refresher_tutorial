c7a3ad8dad5c75a3a747a44e6c7ec19490c46a85
makoegel <marion-koegel@gmx.de>
2024-10-27 17:08:26 +0100

    function with default parameter

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,10 @@
 
-function greet(userName, message) {
+function greet(userName, message = "Hi") {
     console.log(message + " " + userName + "!");
 }
 
 greet("Test1", "Hello");
 greet("Test2", "Hello");
+greet("Test3");
 
 
