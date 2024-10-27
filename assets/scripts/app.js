29a5e06f6bd3b8a31b0f7f297b828f48a6bef6bb
makoegel <marion-koegel@gmx.de>
2024-10-27 17:33:09 +0100

    defining objects with class keyword - blueprint for objects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,11 +4,25 @@ const user = {
     greet() {
         return("Hello" + " " + this.name + "!");
     }
-}
+};
 
 console.log(user);
 console.log("Name:" + user.name + ", Alter:" + user.age);
 console.log(user.greet());
 
+
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet (){
+        return("Hello" + " " + this.name + "!");
+    }
+}
+
+const user1 = new User("Test2", 35);
+console.log(user1.greet());
 
 
