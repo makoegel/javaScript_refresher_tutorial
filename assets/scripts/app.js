30605b5a836d9750e8073a741bdaacc09745a2f8
makoegel <marion-koegel@gmx.de>
2024-10-27 18:16:17 +0100

    control structures for arrays with for loop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,5 @@
 
-const password = prompt("Your Password: ");
-if (password === "Test"){
-  console.log("Password matched");
-}else if (password === "Hello"){
-    console.log("Password works");
-}else {
- console.log("Access not granted.")
+const hobbies= ["Sports", "Cooking"];
+for (const hobby of hobbies) {
+    console.log(hobby)
 }
