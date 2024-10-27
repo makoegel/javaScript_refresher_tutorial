a428c1c84f7af82fcf4ed617d01ed8e8013eede9
makoegel <marion-koegel@gmx.de>
2024-10-27 18:14:51 +0100

    control structures with if statement

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,9 @@
 
-const user = {
-    name: "Test",
-    age: 22
-};
-
-const extendedUser = {
-    isAdmin: true,
-    ...user
+const password = prompt("Your Password: ");
+if (password === "Test"){
+  console.log("Password matched");
+}else if (password === "Hello"){
+    console.log("Password works");
+}else {
+ console.log("Access not granted.")
 }
-
-console.log(extendedUser);
